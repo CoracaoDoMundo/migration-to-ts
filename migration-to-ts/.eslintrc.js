1af290b41a674b84a9f98e89c4954e5bf74a1699
Anna Coração <coracao@Annas-MacBook-Air.local>
2023-06-04 19:38:23 +0300

    chore: add typescript and eslint with configs

--- migration-to-ts/.eslintrc.js
+++ migration-to-ts/.eslintrc.js
@@ -3,17 +3,47 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  //   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  extends: ['airbnb-base', 'plugin:prettier/recommended'],
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript/base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
+    project: './tsconfig.json',
   },
-  //   plugins: ['@typescript-eslint'],
-  plugins: ['prettier'],
+  plugins: ['prettier', '@typescript-eslint'],
+  root: true,
   rules: {
     'import/extensions': 'off',
     'prettier/prettier': 'error',
+    'import/prefer-default-export': 'off',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/array-type': [
+      'error',
+      {
+        default: 'array',
+      },
+    ],
+    '@typescript-eslint/explicit-member-accessibility': [
+      'error',
+      {
+        accessibility: 'explicit',
+        overrides: {
+          accessors: 'explicit',
+          constructors: 'off',
+          methods: 'explicit',
+          properties: 'explicit',
+          parameterProperties: 'explicit',
+        },
+      },
+    ],
+    'max-lines-per-function': ['error', 40],
+    '@typescript-eslint/explicit-function-return-type': 'error',
+    '@typescript-eslint/no-unnecessary-type-assertion': 'error',
+    '@typescript-eslint/no-non-null-assertion': 'error',
   },
 };
