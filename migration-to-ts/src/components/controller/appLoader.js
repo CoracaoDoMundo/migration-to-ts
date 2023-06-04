ef7b6b0efa1dad0e911b1f31e4d14228376cbc19
Anna Coração <coracao@Annas-MacBook-Air.local>
2023-06-04 21:08:04 +0300

    fix: change news api link on proxi lint and add unique api key

--- migration-to-ts/src/components/controller/appLoader.js
+++ migration-to-ts/src/components/controller/appLoader.js
@@ -1,11 +1,11 @@
 import Loader from './loader';
 
 class AppLoader extends Loader {
-    constructor() {
-        super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
-        });
-    }
+  constructor() {
+    super('https://news-proxy.spanb4.shop/', {
+      apiKey: 'dea2647f6bd441ecaa6fc9eeda9bf47d', // получите свой ключ https://newsapi.org/
+    });
+  }
 }
 
 export default AppLoader;
