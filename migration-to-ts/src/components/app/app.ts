0427ab5818f0dede33a6c812feab3af9039ce5ad
Anna Coração <coracao@Annas-MacBook-Air.local>
2023-06-12 01:20:33 +0300

    chore: migrate app.js to app.ts

--- migration-to-ts/src/components/app/app.ts
+++ migration-to-ts/src/components/app/app.ts
@@ -1,6 +1,6 @@
 import AppController from '../controller/controller';
 import { AppView } from '../view/appView';
-import { DataNews } from '../types/index';
+import { DataNews, DataSources } from '../types/index';
 
 class App {
     private controller: AppController;
@@ -11,11 +11,11 @@ class App {
         this.view = new AppView();
     }
 
-    start() {
+    public start() {
         document
-            .querySelector('.sources')
-            .addEventListener('click', (e) => this.controller.getNews(e, (data: DataNews) => this.view.drawNews(data)));
-        this.controller.getSources((data) => this.view.drawSources(data));
+            .querySelector('.sources')!
+            .addEventListener('click', (e: Event) => this.controller.getNews(e, (data: DataNews) => this.view.drawNews(data)));
+        this.controller.getSources((data: DataSources) => this.view.drawSources(data));
     }
 }
 
