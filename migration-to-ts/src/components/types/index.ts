8ee97faaec8fb141bb280f0a3216978bf6c130a1
Anna Coração <coracao@Annas-MacBook-Air.local>
2023-06-12 17:04:13 +0300

    refactor: types and interfaces

--- migration-to-ts/src/components/types/index.ts
+++ migration-to-ts/src/components/types/index.ts
@@ -1,64 +1,88 @@
 type Status = 'ok' | 'error';
 
-type Category = 'business' | 'entertainment' | 'general' | 'health' | 'science' | 'sports' | 'technology';
+enum Category {
+  'business',
+  'entertainment',
+  'general',
+  'health',
+  'science',
+  'sports',
+  'technology',
+}
 
-type Language = 'ar' | 'de' | 'en' | 'es' | 'fr' | 'he' | 'it' | 'nl' | 'no' | 'pt' | 'ru' | 'sv' | 'ud' | 'zh';
+enum Language {
+  'ar',
+  'de',
+  'en',
+  'es',
+  'fr',
+  'he',
+  'it',
+  'nl',
+  'no',
+  'pt',
+  'ru',
+  'sv',
+  'ud',
+  'zh',
+}
 
-type Country =
-  | 'ae'
-  | 'ar'
-  | 'at'
-  | 'au'
-  | 'be'
-  | 'bg'
-  | 'br'
-  | 'ca'
-  | 'ch'
-  | 'cn'
-  | 'co'
-  | 'cu'
-  | 'cz'
-  | 'de'
-  | 'eg'
-  | 'fr'
-  | 'gb'
-  | 'gr'
-  | 'hk'
-  | 'hu'
-  | 'id'
-  | 'ie'
-  | 'il'
-  | 'in'
-  | 'it'
-  | 'jp'
-  | 'kr'
-  | 'lt'
-  | 'lv'
-  | 'ma'
-  | 'mx'
-  | 'my'
-  | 'ng'
-  | 'nl'
-  | 'no'
-  | 'nz'
-  | 'ph'
-  | 'pl'
-  | 'pt'
-  | 'ro'
-  | 'rs'
-  | 'ru'
-  | 'sa'
-  | 'se'
-  | 'sg'
-  | 'si'
-  | 'sk'
-  | 'th'
-  | 'tr'
-  | 'tw'
-  | 'ua'
-  | 'us'
-  | 've'
-  | 'za';
+enum Country {
+  'ae',
+  'ar',
+  'at',
+  'au',
+  'be',
+  'bg',
+  'br',
+  'ca',
+  'ch',
+  'cn',
+  'co',
+  'cu',
+  'cz',
+  'de',
+  'eg',
+  'fr',
+  'gb',
+  'gr',
+  'hk',
+  'hu',
+  'id',
+  'ie',
+  'il',
+  'in',
+  'it',
+  'jp',
+  'kr',
+  'lt',
+  'lv',
+  'ma',
+  'mx',
+  'my',
+  'ng',
+  'nl',
+  'no',
+  'nz',
+  'ph',
+  'pl',
+  'pt',
+  'ro',
+  'rs',
+  'ru',
+  'sa',
+  'se',
+  'sg',
+  'si',
+  'sk',
+  'th',
+  'tr',
+  'tw',
+  'ua',
+  'us',
+  've',
+  'za',
+}
 
 type SourceId = { id: string; name: string };
 
@@ -119,10 +143,3 @@ export interface Response {
   status: Status;
   sources: Source[];
 }
-
-// export interface Request {
-//   apiKey: string;
-//   category?: Category;
-//   language?: Language;
-//   country?: Country;
-// }
